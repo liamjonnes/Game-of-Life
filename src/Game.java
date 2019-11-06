58168400f57027f3b36ce04660a48698e3948bbd
Liam Jones <zeac067@live.rhul.ac.uk>
2019-11-06 13:47:13 +0000

    Added a constructor and JavaDoc
    Added a constructor to allow for a Game object to be created. The constructor takes a size for the grid, then initialises an array of that size. Also added JavaDoc to help explain the purpose of the class and the constructor

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,29 @@
-
+/**
+ * A class that contains the rules of the Game of Life:
+ * 
+ * Scenario 0: No interactions; 
+ * Scenario 1: Underpopulation;
+ * Scenario 2: Overcrowding; 
+ * Scenario 3: Survival;
+ * Scenario 4: Creation of Life; 
+ * Scenario 5: Grid with no live cells;
+ * Scenario 6: Expected game outcome for seeded grid
+ * 
+ * @author Liam Jones
+ */
 public class Game {
 
+  public int gameSize;
+  public int[][] grid;
+  
+  /**
+   * Initialises a grid (array of N x N elements) where
+   * cells will be displayed.
+   * 
+   * @param size  the size that the grid would be
+   */
+  public Game(int size) {
+    this.gameSize = size;
+    this.grid = new int[gameSize][gameSize];
+  }
 }
