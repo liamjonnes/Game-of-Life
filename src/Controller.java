85b70cc3a07b219b26a27ae70740488213b455e5
Liam Jones <zeac067@live.rhul.ac.uk>
2019-11-07 12:27:11 +0000

    Added a timer to iterate and repaint the grid
    Added a new timer that iterates the grid until its next state, and repaints (using the overriden paintComponent() method from the GridDisplay class) the grid once its next state has been determined.

--- src/Controller.java
+++ src/Controller.java
@@ -1,12 +1,27 @@
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Controller {
 
   public Game game;
   public GridDisplay grid;
+  public Timer timer;
   
   public Controller(Game game, GridDisplay grid) {
     this.game = game;
     this.grid = grid;
   }
   
+  public void runGame() {
+    timer = new Timer();
+    
+    timer.schedule(new TimerTask() {
+      @Override
+      public void run() {
+        game.iterateGrid();
+        
+        grid.repaint();
+      }
+    }, 200);
+  }
 }
