e86716bff5dab9e81565e70a01ffb921c4dc0650
Liam Jones <zeac067@live.rhul.ac.uk>
2019-11-07 12:18:43 +0000

    Created a constructor for the Controller
    Initialises the game and the grid variables within the Controller class.

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,12 @@
 
 public class Controller {
 
+  public Game game;
+  public GridDisplay grid;
+  
+  public Controller(Game game, GridDisplay grid) {
+    this.game = game;
+    this.grid = grid;
+  }
+  
 }
