d7761b8e720cae9372abd636ccdbc9fb5d9a5f59
Liam Jones <zeac067@live.rhul.ac.uk>
2019-11-07 10:56:52 +0000

    Added a JFrame, and a choice to run via a GUI or the command line
    Added a while loop that will continue to loop until one of the three options (c, g, or q) is input by the user; once the user inputs one of these, the relevant if statement is chosen, and the chosen user interface is used.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,39 @@
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+
 public class Main {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("How big should the grid be? ");
-    int gridSize = scanner.nextInt();
-    System.out.println("How many states should be printed?");
-    int numberOfStates = scanner.nextInt();
-    
-    Game game = new Game(gridSize);
-    
-    game.printGrid(numberOfStates);
+    while(true) {
+      System.out.println("How would you like to run the program?");
+      System.out.println("(c) for via the command line, (g) for the graphical user interface, or (q) to quit.");
+      String runVia = scanner.next();
+      char userInput = runVia.charAt(0);  // converts the user input to a character
+      
+      if((userInput == 'c') || (userInput == 'C')) {
+        System.out.println("How big should the grid be? ");
+        int gridSize = scanner.nextInt();
+        System.out.println("How many states should be printed?");
+        int numberOfStates = scanner.nextInt();
+        
+        Game game = new Game(gridSize);
+        
+        game.printGrid(numberOfStates);
+        break;
+      } else if((userInput == 'g') || (userInput == 'G')) {
+        JFrame frame = new JFrame("Game of Life");
+        
+        frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.setVisible(true);
+        break;
+      } else if((userInput == 'q') || (userInput == 'Q')) {
+        System.exit(0);
+      }
+    }
   }
-
 }
