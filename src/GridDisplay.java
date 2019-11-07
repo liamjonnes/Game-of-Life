ae1a9b76c0946be879be3b8fdee49ef81961a9f6
Liam Jones <zeac067@live.rhul.ac.uk>
2019-11-07 11:30:55 +0000

    Added a Constructor to GridDisplay
    The constructor initialises a new Game, and also sets the JPanel to be the same size as the JFrame in the Main class (400 x 400), and sets the background of the JPanel colour to white.

--- src/GridDisplay.java
+++ src/GridDisplay.java
@@ -1,4 +1,29 @@
+import java.awt.Color;
+import java.awt.Dimension;
 
-public class GridDisplay {
+import javax.swing.JPanel;
 
+/**
+ * A class that allows for the visualisation of the game in
+ * a Graphical User Interface (Java Swing).
+ * 
+ * @author Liam Jones
+ */
+public class GridDisplay extends JPanel {
+  
+  public Game game;
+
+  /**
+   * Initialises a new grid/game that will be displayed
+   * within a JPanel; the background colour is white, and
+   * thus any dead cells will also be white; any alive
+   * cells will be another colour.
+   * 
+   * @param game  that will get visualised
+   */
+  public GridDisplay(Game game) {
+    this.game = game;
+    setBackground(Color.WHITE)
+    setPreferredSize(new Dimension(400, 400));
+  }
 }
