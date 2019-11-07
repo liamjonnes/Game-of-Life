1f49b493a4b3a30ced2db8bba9fcad35f60ec0aa
Liam Jones <zeac067@live.rhul.ac.uk>
2019-11-07 12:52:02 +0000

    Added JavaDoc for the Controller
    Added JavaDoc for the controller, explaining what the methods and what the class is about.

--- src/Controller.java
+++ src/Controller.java
@@ -1,18 +1,34 @@
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * A controller that links together the model (Game.java),
+ * the view (GridDisplay.java).
+ * 
+ * @author Liam Jones
+ *
+ */
 public class Controller {
 
   public Game game;
   public GridDisplay grid;
   public Timer timer;
   
+  /**
+   * Initialises the controller.
+   * 
+   * @param game  the game to iterate through
+   * @param grid  the grid to paint
+   */
   public Controller(Game game, GridDisplay grid) {
     this.game = game;
     this.grid = grid;
     this.runGame();
   }
   
+  /**
+   * Runs the game, and changes the grid every 0.2 seconds.
+   */
   public void runGame() {
     timer = new Timer();
     
